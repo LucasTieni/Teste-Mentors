f7fce56b6d5d18d3b608925c8fa31f13a974bafe
LucasTieni <54364103+LucasTieni@users.noreply.github.com>
2021-12-06 20:02:29 -0300

    Update README.m

--- README.m
+++ README.m
@@ -1,3 +1,3 @@
 # Teste-Mentors
 
-O nootbook está rodando em uma VM no seguinte (link)[http://34.151.208.60/hub/user-redirect/git-pull?repo=https://github.com/LucasTieni/Teste-Mentors&branch=main&subPath=TesteMentors.ipynb&app=notebook]
+*O nootbook está rodando em uma VM no seguinte (link)[http://34.151.208.60/hub/user-redirect/git-pull?repo=https://github.com/LucasTieni/Teste-Mentors&branch=main&subPath=TesteMentors.ipynb&app=notebook]
