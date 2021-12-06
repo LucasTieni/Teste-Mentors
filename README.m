108cef52863986c1316636b0c63bca12cfe0aaf9
lucastieni <lucastieni@gmail.com>
2021-12-06 20:50:11 -0300

    atualiza-readme

--- README.m
+++
@@ -1,8 +0,0 @@
-# Demo Mentors
-
-### Funções
-
-*italico*
-
-
-O nootbook está rodando em uma VM no seguinte [link](http://34.151.208.60/hub/user-redirect/git-pull?repo=https://github.com/LucasTieni/Teste-Mentors&branch=main&subPath=TesteMentors.ipynb&app=notebook)
